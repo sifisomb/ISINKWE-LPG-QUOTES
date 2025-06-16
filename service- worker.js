c805bb03fcafa07302b78e6f70dddee47e217365
sifisomb <SifisoMB@gmail.com>
2025-06-17 00:56:51 +0200

    Update service- worker.js

--- service- worker.js
+++ service- worker.js
@@ -1,24 +1,43 @@
-const CACHE_NAME = "isinkwe-lpg-cache-v1";
+const CACHE_NAME = 'isinkwe-lpg-cache-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/service-worker.js",
-  "/1000026370.jpg"
+  './',
+  './index.html',
+  './manifest.json',
+  'https://drive.google.com/uc?export=view&id=1wtw8IH2L5W-rqBXf-ui-twG3uZ1AWTQp'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then(networkResponse => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return networkResponse;
+      });
     })
   );
 });
